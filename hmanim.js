cfba20246ba14f9c65c01d1f238ca26fbaeb1a38
Dhruv Chaturvedi <dhruvastro67@gmail.com>
2024-01-23 01:26:38 +0530

    erorr solve

--- hmanim.js
+++ hmanim.js
@@ -1,3 +1,65 @@
-function createRandomNumber(){
-    const rand
+// Initialising the canvas
+var canvas = document.querySelector('canvas'),
+    ctx = canvas.getContext('2d');
+
+// Setting the width and height of the canvas
+var canvasContainer = document.getElementById('nodrop');
+canvas.height = canvasContainer.clientHeight;
+canvas.width = canvasContainer.clientWidth;
+
+
+
+var letters = 'ABCDEFGHIJKLMNOPQRSTUVXYZABCDEFGHIJKLMNOPQRSTUVXYZABCDEFGHIJKLMNOPQRSTUVXYZABCDEFGHIJKLMNOPQRSTUVXYZABCDEFGHIJKLMNOPQRSTUVXYZABCDEFGHIJKLMNOPQRSTUVXYZ';
+letters = letters.split('');
+
+
+
+
+var fontSize = 10;
+
+var columns ;
+var newcoloumn ; 
+var drops = [];
+
+
+columns = Math.floor(canvas.width / fontSize);
+
+for (var i = 0; i < columns; i++) {
+    drops[i] = 1;
 }
+
+window.addEventListener('resize',function(){
+    canvas.width = canvasContainer.clientWidth;
+    
+    columns = Math.floor(canvas.width/fontSize)
+    
+    for (var i = 0; i < columns; i++) {
+        drops[i] = 1;
+    }
+    console.log(drops.length + " , " + columns);
+})
+
+
+function draw() {
+    
+    ctx.fillStyle = 'rgba(0, 0, 0, .1)';
+    ctx.fillRect(0, 0, canvasContainer.clientWidth, canvasContainer.clientHeight);
+    for (var i = 0; i < columns; i++) {
+        var text = letters[Math.floor(Math.random() * letters.length)];
+        ctx.fillStyle = '#0f0';
+        // console.log(text)
+        ctx.fillText(text, i * fontSize, drops[i]* fontSize);
+        drops[i]++;
+        // console.log(drops.length+"huhu")
+        if (drops[i] * fontSize > canvas.height && Math.random() > .95) {
+            drops[i] = 0;
+        }
+    }
+}
+
+// Loop the animation
+setInterval(function () {
+    draw();
+
+},33);
+// initialize();
