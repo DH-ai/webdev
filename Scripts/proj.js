077e87a817307b51924cad186c66cbfb33b80ca3
Dhruv Chaturvedi <dhruvastro67@gmail.com>
2024-01-30 23:48:08 +0530

    added the python server file

--- Scripts/proj.js
+++ Scripts/proj.js
@@ -21,7 +21,7 @@ function proc(){
 
 
 
-
+// buttons for navigations 
 function next(){
     sections[i].style.left = '0px'
     sections[i].style.top = '0px'
@@ -35,3 +35,55 @@ function prev(){
     console.log(i)
     i--;
 }
+
+
+
+// slide show code 
+// console.log(slides)
+
+// const slides = document.querySelectorAll(".sld");
+// console.log(slides)
+// offsetp =0;
+// let ip =1;
+// ordr=0
+// const observer = new IntersectionObserver(function (slide){
+//     slide.forEach(id => {
+        
+//         if (!id.isIntersecting){
+//             id.target.style.right="-100%";
+            
+//             ordr++;
+//             console.log("order changed   "+ id.target.style.offset)
+            
+//         }
+//     });
+// });
+
+// slides.forEach(id=>{
+//     observer.observe(id)
+// })
+// function sld(){
+//     // if (slides.forEach(element=>
+//     //     element.onmouseover))
+    
+//     // slides[2].remove()
+//     slides.forEach(element => {
+//         // element.addEventListener("mouseover",function(){
+//             //     offsetp--;
+//             // })
+//             element.style.right  = offsetp +"px";
+            
+//             offsetp++;
+//             // element.style.position = "fixed"
+            
+            
+            
+//         });
+//         // offsetp++;
+        
+        
+//     // console.log(ip++)
+   
+// }
+
+// setInterval(sld,100)
